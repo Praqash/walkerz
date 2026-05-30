adc362e52129b2e127e2dcd2e3da3860197c8bee
commiting <0praqash0@gmail.com>
2026-05-31 01:54:22 +0530

    Fix mobile footer and map location

--- src/components/sections/MapSection.tsx
+++ src/components/sections/MapSection.tsx
@@ -2,10 +2,11 @@ import DirectionsIcon from "@mui/icons-material/Directions";
 import MapIcon from "@mui/icons-material/Map";
 import SectionHeader from "@/components/ui/SectionHeader";
 
-const googleMapsLink = "https://maps.app.goo.gl/JPJv4i3eM3F6pQ758?g_st=am";
 const mapCoordinates = "26.82942475,80.9960207";
+const googleMapsLink =
+  "https://www.google.com/maps/place/Footsteps+Hostel+and+Cafe/@26.82942475,80.9960207,16z/data=!4m2!3m1!1s0x3904872e14e0ee1d:0x3ef650f083866127";
 const mapEmbedUrl =
-  `https://maps.google.com/maps?q=${mapCoordinates}&ll=${mapCoordinates}&z=16&output=embed`;
+  `https://maps.google.com/maps?hl=en&q=${mapCoordinates}&ll=${mapCoordinates}&z=16&iwloc=B&output=embed`;
 
 export default function MapSection() {
   return (
